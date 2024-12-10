535651aaacac89c5c81e0ddecfdcf33293ce99df
Felix McDougle <flm96@outlook.com>
2024-12-10 10:03:23 +0000

    again

--- lib/src/widgets/actions/panoptic_large_button.dart
+++ lib/src/widgets/actions/panoptic_large_button.dart
@@ -50,7 +50,7 @@ class _PanopticLargeButtonState extends State<PanopticLargeButton> {
           },
           alternative: widget.alternative,
           color: isSelected
-              ? Theme.of(context).colorScheme.primary.withAlpha(255)
+              ? Theme.of(context).colorScheme.primary.withAlpha(55)
               : null,
           child: Column(
             children: [
