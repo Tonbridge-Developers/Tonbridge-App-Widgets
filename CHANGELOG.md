24ad78376554e9bdc8ea99d95faf5629629cc5d9
Felix McDougle <flm96@outlook.com>
2025-03-16 10:59:56 +0000

    Update version to 2025.3.10, integrate gradient elevated button, and enhance button styling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 2025.3.9
+## 2025.3.10
 
 * Fixed button colour issue
   
--- lib/src/widgets/actions/panoptic_button.dart
+++ lib/src/widgets/actions/panoptic_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_elevated_button/gradient_elevated_button.dart';
 import 'package:macos_haptic_feedback/macos_haptic_feedback.dart';
 import 'package:panoptic_widgets/panoptic_widgets.dart';
 import 'package:panoptic_widgets/src/static/core_values.dart';
@@ -60,18 +61,36 @@ class _PanopticButtonState extends State<PanopticButton> {
   Widget build(BuildContext context) {
     return Padding(
       padding: widget.margin,
-      child: MaterialButton(
+      child: GradientElevatedButton(
         onPressed:
             widget.isDisabled || widget.isLoading ? null : widget.onPressed,
         onLongPress:
             widget.isDisabled || widget.isLoading ? null : widget.onLongPress,
-        disabledColor: _getButtonColor(),
+        style: GradientElevatedButton.styleFrom(
+          backgroundGradient: widget.gradient ??
+              LinearGradient(
+                colors: [
+                  _getButtonColor(),
+                  PanopticExtension.shiftHue(_getButtonColor(), 15)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+          disabledBackgroundGradient: widget.gradient ??
+              LinearGradient(
+                colors: [
+                  _getButtonColor(),
+                  PanopticExtension.shiftHue(_getButtonColor(), 15)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+          foregroundColor: _getTextColor(),
+          elevation: widget.elevation,
+          padding: EdgeInsets.zero,
+          shape: _getShape(),
+        ),
         key: widget.key,
-        color: _getButtonColor(),
-        textColor: _getTextColor(),
-        elevation: widget.elevation,
-        padding: EdgeInsets.zero,
-        shape: _getShape(),
         child: MouseRegion(
           onEnter: (event) {
             if (isMacOS) {
@@ -80,21 +99,6 @@ class _PanopticButtonState extends State<PanopticButton> {
           },
           child: Container(
             padding: widget.padding,
-            decoration: BoxDecoration(
-              gradient: (widget.isLoading || widget.expanded)
-                  ? null
-                  : widget.gradient ??
-                      LinearGradient(
-                        colors: [
-                          _getButtonColor(),
-                          PanopticExtension.shiftHue(_getButtonColor(), 15)
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-              borderRadius:
-                  BorderRadius.circular(CoreValues.cornerRadius * 0.8),
-            ),
             child: widget.isLoading
                 ? _buildLoadingIndicator()
                 : _buildButtonContent(),
@@ -110,10 +114,7 @@ class _PanopticButtonState extends State<PanopticButton> {
       mainAxisSize: widget.expanded ? MainAxisSize.max : MainAxisSize.min,
       children: [
         Padding(
-          padding: (PanopticExtension.isWebOrDesktop() ||
-                      Theme.of(context).platform == TargetPlatform.macOS ||
-                      Theme.of(context).platform == TargetPlatform.windows) &&
-                  !widget.smaller
+          padding: (PanopticExtension.isWebOrDesktop()) && !widget.smaller
               ? const EdgeInsets.all(10)
               : EdgeInsets.zero,
           child: SizedBox(
@@ -142,12 +143,7 @@ class _PanopticButtonState extends State<PanopticButton> {
             size: 20,
             margin: widget.label != null
                 ? const EdgeInsets.only(left: 10)
-                : (PanopticExtension.isWebOrDesktop() ||
-                            Theme.of(context).platform ==
-                                TargetPlatform.macOS ||
-                            Theme.of(context).platform ==
-                                TargetPlatform.windows) &&
-                        !widget.smaller
+                : (PanopticExtension.isWebOrDesktop()) && !widget.smaller
                     ? const EdgeInsets.all(10)
                     : EdgeInsets.zero,
           ),
@@ -159,11 +155,7 @@ class _PanopticButtonState extends State<PanopticButton> {
         if (widget.expanded)
           Expanded(
             child: Padding(
-              padding: (PanopticExtension.isWebOrDesktop() ||
-                          Theme.of(context).platform == TargetPlatform.macOS ||
-                          Theme.of(context).platform ==
-                              TargetPlatform.windows) &&
-                      !widget.smaller
+              padding: (PanopticExtension.isWebOrDesktop()) && !widget.smaller
                   ? const EdgeInsets.all(10)
                   : EdgeInsets.zero,
               child: Text(
@@ -178,10 +170,7 @@ class _PanopticButtonState extends State<PanopticButton> {
           )
         else if (widget.label != null)
           Padding(
-            padding: (PanopticExtension.isWebOrDesktop() ||
-                        Theme.of(context).platform == TargetPlatform.macOS ||
-                        Theme.of(context).platform == TargetPlatform.windows) &&
-                    !widget.smaller
+            padding: (PanopticExtension.isWebOrDesktop()) && !widget.smaller
                 ? const EdgeInsets.all(10)
                 : EdgeInsets.zero,
             child: Text(
@@ -240,7 +229,7 @@ class _PanopticButtonState extends State<PanopticButton> {
     }
   }
 
-  ShapeBorder _getShape() {
+  OutlinedBorder _getShape() {
     if (ThemeProvider.controllerOf(context)
         .currentThemeId
         .startsWith('white')) {
